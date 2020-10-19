bcdb974b0bec9aecb6347c4904ddefa5ea893ef8
tylerbertmore <tylercsmith94@gmail.com>
2020-10-20 04:05:32 +0700

    Adds about/contact card layout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('test');
+const tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+const tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="white";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
+}
+showPanel(0,'#4caf50');
