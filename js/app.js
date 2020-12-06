f0f6a661c00a1cf0cfb0f85c911156a1ddac64a9
tylerbertmore <tylercsmith94@gmail.com>
2020-12-06 09:18:59 +0700

    Change page title, add mproject responsiveness

--- js/app.js
+++ js/app.js
@@ -2,10 +2,28 @@
 //                     Variables                
 //---------------------------------------------------
 
+const copyClipboard = document.querySelector('.copyClipboard');
 
 //---------------------------------------------------
 //                     Event Listeners                
 //---------------------------------------------------
 
+copyClipboard.addEventListener('click', copyEmail());
 
+
+function copyEmail() {
+  const copyText = 'tylerbertmore@gmail.com'
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  
+  const emailTooltip = document.querySelector('.emailTooltip');
+  emailTooltip.innerHTML = "Copied: " + copyText.value;
+  outFunc();
+}
+
+function outFunc() {
+  const emailTooltip = document.querySelector('.emailTooltip');
+  emailTooltip.innerHTML = "Copy to clipboard";
+}
 
